a0a474fecb328afea79a66feafd29a611ecf7606
Rob Kendal <38687284+bpk68@users.noreply.github.com>
2023-07-29 11:27:59 +0100

    Adds content to rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,4 +1,6 @@
 import rss from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
+import Markdoc from '@markdoc/markdoc';
 import { blog } from '../lib/markdoc/frontmatter.schema';
 import { readAll } from '../lib/markdoc/read';
 import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from '../config';
@@ -22,19 +24,24 @@ export const get = async () => {
   // https://example.com/ => https://example.com
   baseUrl = baseUrl.replace(/\/+$/g, '');
 
-  const rssItems = sortedPosts.map(({ frontmatter, slug }) => {
-    const title = frontmatter.title;
-    const pubDate = frontmatter.date;
-    const description = frontmatter.description;
-    const link = `${baseUrl}/blog/${slug}`;
+  const rssItems = sortedPosts.map(
+    ({ frontmatter, slug, content: postContent }) => {
+      const title = frontmatter.title;
+      const pubDate = frontmatter.date;
+      const description = frontmatter.description;
+      const link = `${baseUrl}/blog/${slug}`;
+      const htmlContent = Markdoc.renderers.html(postContent);
+      const content = sanitizeHtml(htmlContent);
 
-    return {
-      title,
-      pubDate,
-      description,
-      link,
-    };
-  });
+      return {
+        title,
+        pubDate,
+        description,
+        content,
+        link,
+      };
+    }
+  );
 
   return rss({
     title: SITE_TITLE,
