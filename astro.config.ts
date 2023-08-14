4778e78892ee7cb6201fdb28f37b4a25d276d448
Rob Kendal <38687284+bpk68@users.noreply.github.com>
2023-08-14 13:51:58 +0100

    Adds environment variable for live site

--- astro.config.ts
+++ astro.config.ts
@@ -24,6 +24,7 @@ const LIVE_URL = 'https://robkendal.co.uk';
 const SCRIPT = process.env.npm_lifecycle_script || '';
 const isBuild = SCRIPT.includes('astro build');
 let BASE_URL = LOCALHOST_URL;
+
 // When you're building your site in local or in CI, you could just set your URL manually
 if (isBuild) {
   BASE_URL = LIVE_URL;
@@ -48,9 +49,6 @@ export default defineConfig({
   server: { port: SERVER_PORT },
   site: BASE_URL,
   integrations: [
-    // tailwind({
-    //   applyBaseStyles: false,
-    // }),
     sitemap(),
 		prefetch(),
 		compress({ logger: 1 }),
