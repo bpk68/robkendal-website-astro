f048297ebb7a9da94e12cbd7e9d71113df295311
Rob Kendal <me@robkendal.co.uk>
2024-11-11 14:22:38 +0000

    Fix: add trailing slash to URL path for meta fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,62 +1,68 @@
 export function shuffle<T>(arr: T[]): T[] {
-  return arr.sort(() => Math.random() - 0.5);
+	return arr.sort(() => Math.random() - 0.5);
 }
 
 export function generateSocialImage({
-  title = '',
-  cloudName = 'k-tech-uk',
-  imagePublicID = 'k-tech-social-image_nucvkg.png',
-  cloudinaryUrlBase = 'https://res.cloudinary.com',
-  version = null,
-  titleFont = 'p3h2baj8hz4r4pbuurvg.woff2',
-  titleExtraConfig = '_bold',
-  imageWidth = 1024,
-  imageHeight = 560,
-  textAreaWidth = 800,
-  textAreaHeight = 510,
-  textLeftOffset = 115,
-  textBottomOffset = -50,
-  textColor = 'FDFAF6',
-  titleFontSize = 85,
+	title = "",
+	cloudName = "k-tech-uk",
+	imagePublicID = "k-tech-social-image_nucvkg.png",
+	cloudinaryUrlBase = "https://res.cloudinary.com",
+	version = null,
+	titleFont = "p3h2baj8hz4r4pbuurvg.woff2",
+	titleExtraConfig = "_bold",
+	imageWidth = 1024,
+	imageHeight = 560,
+	textAreaWidth = 800,
+	textAreaHeight = 510,
+	textLeftOffset = 115,
+	textBottomOffset = -50,
+	textColor = "FDFAF6",
+	titleFontSize = 85,
 }): string {
-  // configure social media image dimensions, quality, and format
-  const imageConfig = [
-    `w_${imageWidth}`,
-    `h_${imageHeight}`,
-    'c_fill',
-    'f_auto',
-  ].join(',');
+	// configure social media image dimensions, quality, and format
+	const imageConfig = [
+		`w_${imageWidth}`,
+		`h_${imageHeight}`,
+		"c_fill",
+		"f_auto",
+	].join(",");
 
-  // configure the title text
-  const titleConfig = [
-    `w_${textAreaWidth}`,
-    `h_${textAreaHeight}`,
-    'c_fit',
-    `co_rgb:${textColor}`,
-    'g_west',
-    `x_${textLeftOffset}`,
-    `y_${textBottomOffset}`,
-    `l_text:${titleFont}_${titleFontSize}${titleExtraConfig}:${encodeURIComponent(
-      title
-    )}`,
-  ].join(',');
+	// configure the title text
+	const titleConfig = [
+		`w_${textAreaWidth}`,
+		`h_${textAreaHeight}`,
+		"c_fit",
+		`co_rgb:${textColor}`,
+		"g_west",
+		`x_${textLeftOffset}`,
+		`y_${textBottomOffset}`,
+		`l_text:${titleFont}_${titleFontSize}${titleExtraConfig}:${encodeURIComponent(
+			title,
+		)}`,
+	].join(",");
 
-  // combine all the pieces required to generate a Cloudinary URL
-  const urlParts = [
-    cloudinaryUrlBase,
-    cloudName,
-    'image',
-    'upload',
-    imageConfig,
-    // underlayClonfig,
-    titleConfig,
-    version,
-    imagePublicID,
-  ];
+	// combine all the pieces required to generate a Cloudinary URL
+	const urlParts = [
+		cloudinaryUrlBase,
+		cloudName,
+		"image",
+		"upload",
+		imageConfig,
+		// underlayClonfig,
+		titleConfig,
+		version,
+		imagePublicID,
+	];
 
-  // remove any falsy sections of the URL (e.g. an undefined version)
-  const validParts = urlParts.filter(Boolean);
+	// remove any falsy sections of the URL (e.g. an undefined version)
+	const validParts = urlParts.filter(Boolean);
 
-  // join all the parts into a valid URL to the generated image
-  return validParts.join('/');
+	// join all the parts into a valid URL to the generated image
+	return validParts.join("/");
+}
+
+export function fixTrailingSlashPath(pathname: string) {
+	if (pathname[pathname.length] === "/") return pathname;
+
+	return `${pathname}/`;
 }
